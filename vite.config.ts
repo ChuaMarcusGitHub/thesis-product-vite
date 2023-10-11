82cb07b8f735c8543a5b12d86aaf8123749916b0
Marcus Chua <chuamarcusanselm@gmail.com>
2023-10-11 13:12:12 +0800

    [main] Update vite toml and config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/thesis-marcus-chua/",
     resolve: {
         alias: {
             "@src": path.resolve(__dirname, "./src/"),
